a67f915bcb3ec8bfbabbac2355b8ced11ac0eb47
smeetpatel98 <smeetpatel98@gmail.com>
2022-02-14 15:40:03 +0530

    NEWWW

--- MVCProject/Controllers/CascadingController.cs
+++ MVCProject/Controllers/CascadingController.cs
@@ -9,10 +9,31 @@ namespace MVCProject.Controllers
 {
     public class CascadingController : Controller
     {
+        LoginDBEntities db = new LoginDBEntities();
         // GET: Cascading
         public ActionResult Index()
         {
+            ViewBag.CountryList = new SelectList(GetCountryList(), "CId", "Cname");
+
             return View();
+        }
+
+        public List<country> GetCountryList()
+        {
+            List<country> countries = db.countries.ToList();
+            return countries;
+        }
+        public ActionResult GetSateList(int CId)
+        {
+            List<state> states = db.states.Where(x => x.CId == CId).ToList();
+            ViewBag.Slist = new SelectList(states, "SId", "Sname");
+            return PartialView("DisplayStates");
+        }
+        public ActionResult GetCityList(int SId)
+        {
+            List<city> cities = db.cities.Where(x => x.SId == SId).ToList();
+            ViewBag.Citylist = new SelectList(cities, "Cityid", "Cityname");
+            return PartialView("DisplayCities");
         }
     }
 }
--- MVCProject/Models/User.cs
+++ MVCProject/Models/User.cs
@@ -17,5 +17,6 @@ namespace MVCProject.Models
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string LoginErrorMessage { get; internal set; }
     }
 }
